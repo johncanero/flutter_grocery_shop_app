dda6022362e4e182e30b001437f5d433eea83f86
John Canero <johncanero.official@gmail.com>
2023-06-15 07:40:27 +0800

    Add Logo and Intro Text with Padding

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -6,7 +6,30 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    );
+        body: Column(
+      children: [
+        // logo
+        Padding(
+          padding: const EdgeInsets.only(left: 80.0, top: 120.0, right: 80.0, bottom: 80.0),
+          child: Image.asset('lib/images/avocado.png'),
+        ),
+
+        // we deliver groceries at your doorstep
+        const Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Text(
+            "We deliver groceries at your doorstep",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        // fresh items everyday
+
+        // get started button
+      ],
+    ));
   }
 }
